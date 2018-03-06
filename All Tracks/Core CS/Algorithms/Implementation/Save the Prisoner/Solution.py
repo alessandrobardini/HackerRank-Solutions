25247634b7502483cfee56cb10e0f3c665e5b406
Alessandro Bardini <alessandro.bardini93@gmail.com>
2018-03-06 17:32:36 +0100

    modified comments

--- All Tracks/Core CS/Algorithms/Implementation/Save the Prisoner/Solution.py
+++ All Tracks/Core CS/Algorithms/Implementation/Save the Prisoner/Solution.py
@@ -3,8 +3,10 @@
 import sys
 
 """
-Starting from the prisoner s, if the number of prisoners were infinite , the last sweet would be given to the prisoner at index s - 1 + m.
-Since the number of prisoners if limited, when we reach the last prisoner (i.e. the prisoner at index n), we have to come back to prisoner at index 1.
+Starting from the prisoner s, if the number of prisoners were infinite , the last sweet would be given 
+to the prisoner at index s - 1 + m.
+Since the number of prisoners if limited, when we reach the last prisoner (i.e. the prisoner at index n),
+we have to come back to prisoner at index 1.
 So it's necessary to use the modulus operation ( % n).
 If the lastSweetIndex is 0, the last sweet is given to prisoner at index n.
 """
