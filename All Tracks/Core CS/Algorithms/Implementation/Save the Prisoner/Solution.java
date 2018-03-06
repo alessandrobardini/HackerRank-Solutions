c2a1d781ceb16d28fe06b359b7e565f3751f36e6
Alessandro Bardini <alessandro.bardini93@gmail.com>
2018-03-06 17:30:33 +0100

    'Save the Prisoner'

---
+++ All Tracks/Core CS/Algorithms/Implementation/Save the Prisoner/Solution.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+/*
+ * Starting from the prisoner s, if the number of prisoners were infinite , the last sweet would be given to the prisoner at index s - 1 + m.
+ * Since the number of prisoners if limited, when we reach the last prisoner (i.e. the prisoner at index n), we have to come back to prisoner at index 1.
+ * So it's necessary to use the modulus operation ( % n).
+ * If the lastSweetIndex is 0, the last sweet is given to prisoner at index n.
+ */
+
+public class Solution {
+	
+	public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+		
+		int testCases = scanner.nextInt();
+		
+		for (int i = 0; i < testCases; i++) {
+			int n = scanner.nextInt();
+			int m = scanner.nextInt();
+			int s = scanner.nextInt();
+			int lastSweetIndex = (s - 1 + m) % n;
+            System.out.println(lastSweetIndex == 0 ? n : lastSweetIndex);
+		}
+        
+        scanner.close();
+	}
+
+}
---
+++ All Tracks/Core CS/Algorithms/Implementation/Save the Prisoner/Solution.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+
+import sys
+
+"""
+Starting from the prisoner s, if the number of prisoners were infinite , the last sweet would be given to the prisoner at index s - 1 + m.
+Since the number of prisoners if limited, when we reach the last prisoner (i.e. the prisoner at index n), we have to come back to prisoner at index 1.
+So it's necessary to use the modulus operation ( % n).
+If the lastSweetIndex is 0, the last sweet is given to prisoner at index n.
+"""
+
+if __name__ == '__main__':
+    t = int(input().strip())
+    for a0 in range(t):
+        n, m, s = map(int, input().strip().split(' '))
+        last_sweet_index = (s - 1 + m) % n
+        print(last_sweet_index if last_sweet_index != 0 else n)
