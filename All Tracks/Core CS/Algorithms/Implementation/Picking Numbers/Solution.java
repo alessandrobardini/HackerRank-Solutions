19ff191e7a13f745fdd1248966548653d1fb43b5
Alessandro Bardini <alessandro.bardini93@gmail.com>
2018-03-06 10:30:58 +0100

    'Picking Numbers'

---
+++ All Tracks/Core CS/Algorithms/Implementation/Picking Numbers/Solution.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+/*
+ * The condition "The absolute difference between any two of the chosen integers is <= 1" means that the number we choose are:
+ * a) integer with identical value (absolute difference == 0) 
+ * or 
+ * b) consecutive integers (absolute difference == 1).
+ * So it is a good idea to create a frequency array that counts the occurrences of the numbers and then 
+ * iterate through the array to find the maximum value between consecutive elements (i.e. consecutive frequencies).
+ * In this way, we handle case a) and case b).
+ *
+ * Example case a):
+ * 6
+ * 1 1 1 4 5 6 
+ * The frequency array is [3, 0, 0, 1, 1, 1] and the maximum value is 3 (3 + 0)
+ *
+ * Example case b):
+ * 6
+ * 1 1 2 3 5 2 
+ * The frequency array is [2, 2, 1, 1, 0] and the maximum value is 4 (2 + 2)
+ * 
+ */
+
+public class Solution {
+
+    static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		
+		int n = sc.nextInt();
+		int[] freq = new int[100];
+		for (int i = 0; i < n; i++) {
+			freq[sc.nextInt()] += 1;
+		}
+		int max = 0;
+		for (int i = 1; i < freq.length; i++) {
+			max = Math.max(max, freq[i] + freq[i - 1]);
+		}
+		
+		System.out.println(max);
+	}
+}
---
+++ All Tracks/Core CS/Algorithms/Implementation/Picking Numbers/Solution.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import sys
+
+"""
+The condition "The absolute difference between any two of the chosen integers is <= 1" means that the number we choose are:
+a) integer with identical value (absolute difference == 0) 
+or 
+b) consecutive integers (absolute difference == 1).
+So it is a good idea to create a frequency array that counts the occurrences of the numbers and then 
+iterate through the array to find the maximum value between consecutive elements (i.e. consecutive frequencies).
+In this way, we handle case a) and case b).
+
+Example case a):
+6
+1 1 1 4 5 6 
+The frequency array is [3, 0, 0, 1, 1, 1] and the maximum value is 3 (3 + 0)
+
+Example case b):
+6
+1 1 2 3 5 2 
+The frequency array is [2, 2, 1, 1, 0] and the maximum value is 4 (2 + 2)
+"""
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    array = list(map(int, input().strip().split(' ')))
+    frequency = [0] * 100
+    max_value = 0
+    for number in array:
+        frequency[number] += 1
+    for i in range(1, n):
+        max_value = max(max_value, frequency[i] + frequency[i - 1])
+    print(max_value)
