e600175fe07963263244f5d3290edb4d08301349
alessandro <alessandro.bardini93@gmail.com>
2018-03-14 23:30:53 +0100

    comments

--- All Tracks/Core CS/Algorithms/Implementation/Sherlock and Squares/Solution.py
+++ All Tracks/Core CS/Algorithms/Implementation/Sherlock and Squares/Solution.py
@@ -6,13 +6,12 @@ import math
 '''
 The easiest way is to iterate through the range and, for every number, check if that number is a perfect square.
 Unfortunately, this approach leads to a TLE.
-Instead of searching for the perfect squares, we can reverse the problem and search the integers whose square is
+Instead of searching for the perfect squares, we can reverse the problem and search for the integers whose square is
 included in the initial range.
 In this way, we define a lower_limit as the smallest integer whose square is greater than 'a' and an upper_limit 
 as the biggest integer whose square is smaller than 'b'.
 The number of integers included in the range [a, b] is the solution.
 Time complexity: O(1)
-
 '''
 
 if __name__ == "__main__":
