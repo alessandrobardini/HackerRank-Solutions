5f468915f89256830a927ddb4dd4967bd2e86dda
Saarthak Sangamnerkar <31529049+saarthaksangam@users.noreply.github.com>
2020-04-18 16:11:15 +0530

    Update Solution.py
    added a constraint

--- All Tracks/Core CS/Algorithms/Implementation/Drawing Book/Solution.py
+++ All Tracks/Core CS/Algorithms/Implementation/Drawing Book/Solution.py
@@ -9,7 +9,8 @@ So it is sufficient to determine the minimum between these two values.
 """
 
 def solve(n, p):
-    return min(p//2, n//2 - p//2)
+    if(p<=n):
+        return min(p//2, n//2 - p//2)
 
 n = int(input().strip())
 p = int(input().strip())
